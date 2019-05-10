8c7e7b56361dfa1e14beb36a54563bd7b5f08280
IvanMejico <ivan.mejico21@gmail.com>
2019-05-10 14:45:47 +0800

    Add and Delete books

--- app.js
+++ app.js
@@ -47,6 +47,24 @@ class UI {
         list.appendChild(row);
     }
 
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+             el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -68,14 +86,29 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instantiate book
-    const book = new Book(title, author, isbn);
+    //Validate
+    if(title === '' || author === '' || isbn === '') {
+        // alert('Please fill in all fields');
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        //Instantiate book
+        const book = new Book(title, author, isbn);
 
-    // Add book to UI
-    UI.addBookToList(book);
+        // Add book to UI
+        UI.addBookToList(book);
 
-    //Clear fields
-    UI.clearFields();
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+
+        //Clear fields
+        UI.clearFields();
+    }
 });
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
+});
